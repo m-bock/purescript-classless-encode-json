a3273d272496089946f01d0190f628a77b191311
Michael Bock <no-day@posteo.net>
2022-11-26 16:51:32 +0100

    Remove unused code

---
+++ README.md
@@ -0,0 +1,5 @@
+# purescript-classless-encode-json
+
+
+This library applies the "classless" concept to the encoding strategy implemented in [argonaout-generic](https://github.com/purescript-contrib/purescript-argonaut-generic). Most of the encoding code is taken from there.
+Currently the [classless-arbitrary](https://github.com/thought2/purescript-classless-arbitrary) package is best documented and is recommended for getting started with the classless API concept. It will be helpful for understand this package, too.
--- src/Classless/EncodeJson/Generic.purs
+++ src/Classless/EncodeJson/Generic.purs
@@ -1,23 +1,13 @@
 module Classless.EncodeJson.Generic
-  ( class EncodeLiteral
-  , class EncodeRep
-  , class EncodeRepArgs
-  , class Sum
-  , encodeLiteral
-  , encodeLiteralSum
-  , encodeLiteralSumWithTransform
-  , encodeRep
-  , encodeRepArgs
-  , encodeRepWith
+  ( class Sum
   , sum
-  , sumWith
   ) where
 
 import Prelude
 
 import Classless (type (~), NoArgs, (~))
 import Data.Argonaut.Core (Json, fromArray, fromObject, fromString)
-import Data.Generic.Rep (class Generic)
+import Data.Generic.Rep (class Generic, NoArguments)
 import Data.Generic.Rep as Rep
 import Data.Symbol (class IsSymbol, reflectSymbol)
 import Foreign.Object as FO
@@ -44,16 +34,13 @@ defaultEncoding =
 class EncodeRep sumSpec r | r -> sumSpec where
   encodeRepWith :: { | sumSpec } -> Encoding -> r -> Json
 
-encodeRep :: forall sumSpec r. EncodeRep sumSpec r => { | sumSpec } -> r -> Json
-encodeRep sumSpec = encodeRepWith sumSpec defaultEncoding
-
-instance encodeRepNoConstructors :: EncodeRep () Rep.NoConstructors where
+instance EncodeRep () Rep.NoConstructors where
   encodeRepWith e = encodeRepWith e
 
-instance encodeRepSum ::
+instance
   ( TypeEquals a (Rep.Constructor name xx)
   , Cons name x sumSpec' sumSpec
-  , Cons name x () sumSpec'' 
+  , Cons name x () sumSpec''
   , EncodeRep sumSpec'' a
   , EncodeRep sumSpec' b
   , IsSymbol name
@@ -63,19 +50,19 @@ instance encodeRepSum ::
   encodeRepWith sp e (Rep.Inl a) = encodeRepWith (Record.insert (Proxy :: _ name) (Record.get (Proxy :: _ name) sp) {}) e a
   encodeRepWith sp e (Rep.Inr b) = encodeRepWith (Record.delete (Proxy :: _ name) sp) e b
 
--- instance encodeRepConstructorNoArguments ::
---   ( IsSymbol name
---   ) =>
---   EncodeRep sumSpec (Rep.Constructor name NoArguments) where
---   encodeRepWith _ e (Rep.Constructor a) =
---     fromObject
---       $ FO.insert e.tagKey (fromString (reflectSymbol (Proxy :: Proxy name)))
---       $ FO.insert e.valuesKey values
---       $ FO.empty
---     where
---     values = fromArray []
+instance
+  ( IsSymbol name
+  ) =>
+  EncodeRep sumSpec (Rep.Constructor name NoArguments) where
+  encodeRepWith _ e (Rep.Constructor a) =
+    fromObject
+      $ FO.insert e.tagKey (fromString (reflectSymbol (Proxy :: Proxy name)))
+      $ FO.insert e.valuesKey values
+      $ FO.empty
+    where
+    values = fromArray []
 
-instance encodeRepConstructor ::
+else instance
   ( IsSymbol name
   , EncodeRepArgs prodSpec a
   , Cons name prodSpec () sumSpec
@@ -100,15 +87,14 @@ instance encodeRepConstructor ::
 class EncodeRepArgs prodSpec r | r -> prodSpec where
   encodeRepArgs :: prodSpec -> r -> Array Json
 
-instance encodeRepArgsNoArguments :: EncodeRepArgs NoArgs Rep.NoArguments where
+instance EncodeRepArgs NoArgs Rep.NoArguments where
   encodeRepArgs _ Rep.NoArguments = []
 
-instance encodeRepArgsProduct :: (EncodeRepArgs sa a, EncodeRepArgs sb b) => EncodeRepArgs (sa ~ sb) (Rep.Product a b) where
+instance (EncodeRepArgs sa a, EncodeRepArgs sb b) => EncodeRepArgs (sa ~ sb) (Rep.Product a b) where
   encodeRepArgs (spA ~ spB) (Rep.Product a b) = encodeRepArgs spA a <> encodeRepArgs spB b
 
-instance encodeRepArgsArgument :: EncodeRepArgs (a -> Json) (Rep.Argument a) where
+instance EncodeRepArgs (a -> Json) (Rep.Argument a) where
   encodeRepArgs spec (Rep.Argument a) = [ spec a ]
-
 
 class Sum sumSpec a | a -> sumSpec where
   sum :: { | sumSpec } -> (a -> Json)
@@ -116,34 +102,23 @@ class Sum sumSpec a | a -> sumSpec where
 instance (Generic a rep, EncodeRep sumSpec rep) => Sum sumSpec a where
   sum spec = sumWith spec defaultEncoding
 
--- | Encode any `Generic` data structure into `Json`.
--- | Takes a record for encoding settings.
 sumWith :: forall sumSpec a r. Rep.Generic a r => EncodeRep sumSpec r => { | sumSpec } -> Encoding -> a -> Json
 sumWith spec e = encodeRepWith spec e <<< Rep.from
-
--- | A function for encoding `Generic` sum types using string literal representations.
-encodeLiteralSum :: forall a r. Rep.Generic a r => EncodeLiteral r => a -> Json
-encodeLiteralSum = encodeLiteralSumWithTransform identity
-
--- | A function for encoding `Generic` sum types using string literal representations.
--- | Takes a function for transforming the tag name in encoding.
-encodeLiteralSumWithTransform :: forall a r. Rep.Generic a r => EncodeLiteral r => (String -> String) -> a -> Json
-encodeLiteralSumWithTransform tagNameTransform = encodeLiteral tagNameTransform <<< Rep.from
 
 class EncodeLiteral r where
   encodeLiteral :: (String -> String) -> r -> Json
 
-instance encodeLiteralSumInst :: (EncodeLiteral a, EncodeLiteral b) => EncodeLiteral (Rep.Sum a b) where
+instance (EncodeLiteral a, EncodeLiteral b) => EncodeLiteral (Rep.Sum a b) where
   encodeLiteral tagNameTransform (Rep.Inl a) = encodeLiteral tagNameTransform a
   encodeLiteral tagNameTransform (Rep.Inr b) = encodeLiteral tagNameTransform b
 
-instance encodeLiteralConstructor :: (IsSymbol name) => EncodeLiteral (Rep.Constructor name Rep.NoArguments) where
+instance (IsSymbol name) => EncodeLiteral (Rep.Constructor name Rep.NoArguments) where
   encodeLiteral tagNameTransform _ = fromString <<< tagNameTransform $ reflectSymbol (Proxy :: Proxy name)
 
 type FailMessage =
   Text """`encodeLiteralSum` can only be used with sum types, where all of the constructors are nullary. This is because a string literal cannot be encoded into a product type."""
 
-instance encodeLiteralConstructorCannotBeProduct ::
+instance
   Fail FailMessage =>
   EncodeLiteral (Rep.Product a b) where
   encodeLiteral _ _ = unsafeCrashWith "unreachable encodeLiteral was reached."
